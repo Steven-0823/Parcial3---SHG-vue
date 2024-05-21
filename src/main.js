ff94087ced2a56065b9571e544a2e057a91febab
Joan Steven Henao Bermeo <henaobermeosteven@gmail.com>
2024-05-20 19:02:42 -0500

    Inicio de vista para el parcial

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
-import Vue from 'vue'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBank,faBuilding,faPencil,faPhone,faPlus,faTag,faTrash,faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { createApp } from 'vue';
+
 import App from './App.vue'
 import router from './router'
+library.add(faPhone);
+library.add(faUser);
+library.add(faTrash);
+library.add(faPencil);
+library.add(faTag);
+library.add(faBuilding);
+library.add(faBank);
+library.add(faPlus);
 
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+createApp(App).component("font-awesome-icon",FontAwesomeIcon).use(router).mount('#app')
+// createApp(App).use(router).mount('#app')
